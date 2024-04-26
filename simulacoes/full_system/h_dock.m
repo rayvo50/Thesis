3f4227b5255cd75e8e0f51b783ed69e0c94c3bf1
rayvo50 <raviregalo@gmail.com>
2024-04-26 10:13:10 +0100

    .....

--- simulacoes/full_system/h_dock.m
+++
@@ -1,8 +0,0 @@
-function y = h_dock(x_nav, x_dock)
-    y = zeros(3,1);
-    y(1) = sqrt((x_nav(1)-x_dock(1))^2 + (x_nav(2)-x_dock(2))^2);
-    y(2) = atan2d(x_dock(2)-x_nav(2), x_dock(1)-x_nav(1)) - x_nav(4);
-    y(2) = wrapTo180(y(2));
-    y(3) = atan2d(x_nav(2)-x_dock(2), x_nav(1)-x_dock(1)) - x_dock(3);
-    y(3) = wrapTo180(y(3));
-end
