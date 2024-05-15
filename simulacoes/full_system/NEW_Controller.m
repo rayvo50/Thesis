efcae17170622f8ecb75f19925ccef6d2fd7c1a2
ist196305 <ravi.regalo@tecnico.ulisboa.pt>
2024-05-16 00:28:17 +0100

    :(

--- simulacoes/full_system/NEW_Controller.m
+++ simulacoes/full_system/NEW_Controller.m
@@ -1,4 +1,4 @@
-classdef LOS_Controller_r < handle
+classdef NEW_Controller_r < handle
     properties
         %parameters:
         K_delta
@@ -13,15 +13,17 @@ classdef LOS_Controller_r < handle
         state
         yaw_corr
         output
+
+        debug
         
     end
     
     methods
-        function self = LOS_Controller_r(Dt)
+        function self = NEW_Controller_r(Dt)
             self.K_delta = 10;
-            self.K_phi = 1;
+            self.K_phi = 0.1;
             self.K_e=-1;
-            self.alpha = rad2deg(10);
+            self.alpha = deg2rad(10);
             self.home_D = 20;
             self.R = 20;
             self.Dt = Dt;
@@ -29,6 +31,8 @@ classdef LOS_Controller_r < handle
             self.state=1;
             self.yaw_corr = 0;
             self.output =[0;0;NaN];
+
+            self.debug = [0;0];
         end
         
         function self = compute(self, x, y)
@@ -46,16 +50,16 @@ classdef LOS_Controller_r < handle
             elseif self.state == 1
                 x_home = x(6) + self.home_D*cos(x(8));
                 y_home = x(7) + self.home_D*sin(x(8));
-                yaw_home = atan2(y_home-x(2),x_home-x(1))
+                yaw_home = atan2(y_home-x(2),x_home-x(1));
                 
                 % obstacle avoidance with bearing measurement:
                 bearing = y(5);
                 distance = y(4);
-                if (0<bearing) && (bearing<rad2deg(45)) && (distance<20)
-                    phi = (rad2deg(45)-bearing)/rad2deg(45)*self.K_phi/distance*10;
+                if (0<bearing) && (bearing<deg2rad(45)) && (distance<20)
+                    phi = (deg2rad(45)-bearing)/deg2rad(45)*self.K_phi/distance;
                     self.yaw_corr = self.yaw_corr - phi*self.Dt;
-                elseif (-rad2deg(45)<bearing) && (bearing<0) && (distance<20)  
-                    phi = (bearing+rad2deg(45))/rad2deg(45)*self.K_phi/distance*10;
+                elseif (-deg2rad(45)<bearing) && (bearing<0) && (distance<20)  
+                    phi = (bearing+deg2rad(45))/deg2rad(45)*self.K_phi/distance;
                     self.yaw_corr = self.yaw_corr + phi*self.Dt;
                 elseif self.yaw_corr > 0
                     self.yaw_corr = max(0,self.yaw_corr -2*self.K_phi*self.Dt);
@@ -64,16 +68,11 @@ classdef LOS_Controller_r < handle
                 end
                 yaw_d = wrapToPi(yaw_home+self.yaw_corr);
                 self.output = [0.3;yaw_d;NaN];
+                self.debug = [yaw_home, self.yaw_corr];
         
             % dock path following
             elseif self.state ==2
-                Delta = 5; %min(K_delta*exp(-cross_track), 5);
-                x_los = x(9)-Delta;
-                y_los = 0;
-
-                yaw_des = atan2(y_los-x(10),x_los-x(9));
-            
-                self.output = real([0.3;NaN;yaw_des]);
+                %bruh
             end
         end
 
---
+++ simulacoes/full_system/sway_PID_controller.m
@@ -0,0 +1,94 @@
+classdef sway_PID_controller < handle
+    properties
+        %parameters:
+        K_p
+        K_i
+        K_r
+        K_a
+        lpf_A
+        lpf_B
+        Dt
+        tau_max
+
+        %variables:
+        first_it
+        sway_rate_prev
+        sway_prev
+        g_filter_prev
+        u_prev
+        u_sat_prev
+        output
+        
+    end
+    
+    methods
+        function self = sway_PID_controller()
+            % Parameters
+            Y_v = -0.2;
+            m_v = 30+20;
+            self.tau_max = 10.0; % N.m
+            a = 10.0; % rad/s
+
+            w_n = 5; % rad/s
+            qsi = 0.8;
+        
+            % self.Dt calculation
+            self.Dt = 0.1;
+
+            self.K_a = 1.0 / self.Dt;
+            self.K_p = 2*qsi*w_n*m_v + Y_v;
+            self.K_i = w_n^2*m_v;
+        
+            self.lpf_A = exp(-a*self.Dt);
+            self.lpf_B = 1 - self.lpf_A;
+
+            % variables
+            self.sway_rate_prev = 0;
+            self.sway_prev= 0;
+            self.g_filter_prev=0;
+            self.u_sat_prev=0;
+            self.u_prev=0;
+            self.first_it = true;
+            self.output = 0;
+        end
+        
+        function self = compute(self, sway, sway_ref)
+            error = wrapTo180(sway_ref - sway);
+            
+            sway_rate = (sway - self.sway_prev)/self.Dt;
+            if self.first_it
+                sway_dot = 0;
+            else
+                sway_dot = wrapToPi(sway - self.sway_prev) / self.Dt;
+            end
+        
+            g = self.K_p * sway_dot;
+            g_filter = self.lpf_A * self.g_filter_prev + g * self.lpf_B;
+        
+            u_d = self.K_i * error - g_filter;
+            u_dot = u_d - self.K_a * (self.u_prev - self.u_sat_prev);
+            u = self.u_prev + u_dot * self.Dt;
+        
+            if u < -self.tau_max
+                u_sat = -self.tau_max;
+            elseif u > self.tau_max
+                u_sat = self.tau_max;
+            else
+                u_sat = u;
+            end
+            self.output = u_sat;
+        
+            % Update new prev values
+            self.sway_rate_prev = sway_rate;
+            self.sway_prev = sway;
+            self.g_filter_prev = g_filter;
+            self.u_prev = u;
+            self.u_sat_prev = u_sat; 
+ 
+            self.first_it = false;
+           
+        end
+
+      
+    end
+end
